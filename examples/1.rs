2cbef3709090e688056a2a2be75424b284a188ca
wujinchi <jinchi@inconnecting>
2022-09-19 13:36:53 +0800

    feat: update env

---
+++ examples/1.rs
@@ -0,0 +1,21 @@
+/*
+ * @Author: wujinchi
+ * @Date: 2022-09-19 11:41:25
+ * @LastEditors: wujinchi
+ * @LastEditTime: 2022-09-19 11:44:28
+ * @Description: file content
+ */
+
+// 填空
+
+fn main() {
+    let n = 5;
+
+    if n < 0 {
+        println!("{} is negative", n);
+    } else if n > 0 {
+        println!("{} is positive", n);
+    } else {
+        println!("{} is zero", n);
+    }
+} 
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+
+// #![allow(unused)]
 fn main() {
-    println!("Hello, world!");
+/// `add_one` 将指定值加1
+///
+/// # Examples11
+///
+/// ```
+/// let arg = 5;
+/// let answer = world_hello::compute::add_one(arg);
+///
+/// assert_eq!(6, answer);
+/// ```
+pub fn add_one(x: i32) -> i32 {
+    x + 1
+}
 }
